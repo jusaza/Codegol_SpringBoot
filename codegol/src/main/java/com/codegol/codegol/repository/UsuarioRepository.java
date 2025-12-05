a97b873e31e1c0eed54fdd2cd95c39fdb55ef676
sofilozano-7968 <sofilozano796@gmail.com>
2025-12-05 14:58:42 -0500

    Me permitio poner en lista los jugadores que estan registrados en la matricula

--- codegol/src/main/java/com/codegol/codegol/repository/UsuarioRepository.java
+++ codegol/src/main/java/com/codegol/codegol/repository/UsuarioRepository.java
@@ -7,8 +7,14 @@ package com.codegol.codegol.repository;
 import com.codegol.codegol.model.Usuario;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UsuarioRepository extends JpaRepository <Usuario, Integer> {
     Usuario findByCorreo(String correo);
     List<Usuario> findByEstado(boolean estado);
+
+    @Query("SELECT u FROM Usuario u JOIN u.roles r WHERE r.rol_usuario = :rol AND u.estado = true")
+    List<Usuario> findJugadoresActivos(@Param("rol") String rol);
+
 }
