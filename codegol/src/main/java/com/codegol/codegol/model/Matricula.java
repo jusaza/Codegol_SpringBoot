3696d63b34c3690959c65a696412ad00b8c5dfcb
sofilozano-7968 <sofilozano796@gmail.com>
2025-12-05 15:02:56 -0500

    Agregue los publics y get-set
    Faltaban dos campos en el modulo matricula (categoria y nivel)
    Por lo tanto agregue el public, los get y set

--- codegol/src/main/java/com/codegol/codegol/model/Matricula.java
+++ codegol/src/main/java/com/codegol/codegol/model/Matricula.java
@@ -7,131 +7,133 @@ import jakarta.validation.constraints.Positive;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Entity
 @Table(name= "Matricula")
 public class Matricula {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private int id_matricula;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_matricula;
 
-  @NotNull
-  @DateTimeFormat(pattern = "yyyy-MM-dd")
-  private LocalDate fecha_matriucula;
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate fecha_matricula;
 
-  @NotNull
-  @DateTimeFormat(pattern = "yyyy-MM-dd")
-  private LocalDate fecha_inicio;
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate fecha_inicio;
 
-  @NotNull
-  @DateTimeFormat(pattern = "yyyy-MM-dd")
-  private LocalDate fecha_fin;
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate fecha_fin;
 
-  private boolean estado = true;
+    private boolean estado = true;
 
-  private String observaciones;
+    private String observaciones;
 
-  @NotNull
-  @Positive
-  private  int categoria;
+    @NotBlank(message = "Seleccione una categoría")
+    private String categoria;
 
-  public enum nivel {
-    Bajo,
-    Medio,
-    Alto
-  }
+    public String getCategoria() {
+        return categoria;
+    }
 
-  @Enumerated(EnumType.STRING)
-  @Column(name = "nivel", nullable = false)
-  private nivel nivel;
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 
-  @ManyToOne
-  @JoinColumn(name = "id_jugador")
-  private Usuario id_jugador;
+    public enum nivel {
+        Bajo,
+        Medio,
+        Alto
+    }
 
-  @ManyToOne
-  @JoinColumn(name = "id_usuario")
-  private Usuario id_usuario;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "nivel", nullable = false)
+    private nivel nivel;
 
-  public int getId_matricula() {
-    return id_matricula;
-  }
+    @ManyToOne
+    @JoinColumn(name = "id_jugador")
+    private Usuario id_jugador;
 
-  public void setId_matricula(int id_matricula) {
-    this.id_matricula = id_matricula;
-  }
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario id_usuario;
 
-  public LocalDate getFecha_matriucula() {
-    return fecha_matriucula;
-  }
+    // GETTERS Y SETTERS
 
-  public void setFecha_matriucula(LocalDate fecha_matriucula) {
-    this.fecha_matriucula = fecha_matriucula;
-  }
 
-  public LocalDate getFecha_inicio() {
-    return fecha_inicio;
-  }
+    public int getId_matricula() {
+        return id_matricula;
+    }
 
-  public void setFecha_inicio(LocalDate fecha_inicio) {
-    this.fecha_inicio = fecha_inicio;
-  }
+    public void setId_matricula(int id_matricula) {
+        this.id_matricula = id_matricula;
+    }
 
-  public LocalDate getFecha_fin() {
-    return fecha_fin;
-  }
+    public LocalDate getFecha_matricula() {
+        return fecha_matricula;
+    }
 
-  public void setFecha_fin(LocalDate fecha_fin) {
-    this.fecha_fin = fecha_fin;
-  }
+    public void setFecha_matricula(LocalDate fecha_matricula) {
+        this.fecha_matricula = fecha_matricula;
+    }
 
-  public boolean isEstado() {
-    return estado;
-  }
+    public LocalDate getFecha_inicio() {
+        return fecha_inicio;
+    }
 
-  public void setEstado(boolean estado) {
-    this.estado = estado;
-  }
+    public void setFecha_inicio(LocalDate fecha_inicio) {
+        this.fecha_inicio = fecha_inicio;
+    }
 
-  public String getObservaciones() {
-    return observaciones;
-  }
+    public LocalDate getFecha_fin() {
+        return fecha_fin;
+    }
 
-  public void setObservaciones(String observaciones) {
-    this.observaciones = observaciones;
-  }
+    public void setFecha_fin(LocalDate fecha_fin) {
+        this.fecha_fin = fecha_fin;
+    }
 
-  public int getCategoria() {
-    return categoria;
-  }
+    public boolean isEstado() {
+        return estado;
+    }
 
-  public void setCategoria(int categoria) {
-    this.categoria = categoria;
-  }
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
 
-  public nivel getNivel() {
-    return nivel;
-  }
+    public String getObservaciones() {
+        return observaciones;
+    }
 
-  public void setNivel(nivel nivel) {
-    this.nivel = nivel;
-  }
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
 
-  public Usuario getId_jugador() {
-    return id_jugador;
-  }
 
-  public void setId_jugador(Usuario id_jugador) {
-    this.id_jugador = id_jugador;
-  }
+    public nivel getNivel() {
+        return nivel;
+    }
 
-  public Usuario getId_usuario() {
-    return id_usuario;
-  }
+    public void setNivel(nivel nivel) {
+        this.nivel = nivel;
+    }
 
-  public void setId_usuario(Usuario id_usuario) {
-    this.id_usuario = id_usuario;
-  }
+    public Usuario getId_jugador() {
+        return id_jugador;
+    }
+
+    public void setId_jugador(Usuario id_jugador) {
+        this.id_jugador = id_jugador;
+    }
+
+    public Usuario getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(Usuario id_usuario) {
+        this.id_usuario = id_usuario;
+    }
 }
